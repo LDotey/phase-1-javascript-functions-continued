62f49b209e1f06a3cf96f524eae0407d7116f0fc
Leigh Dotey <leighdotey@gmail.com>
2023-11-27 12:24:46 -0500

    functions wrapped

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity= 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+function mondayWork (activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`)
+}
+function wrapAdjective (quality = 'special'){
+    return (`You are ${quality}!`);
+}
+function wrapAdjective(wrapper = '*'){
+    return function(quality = 'special'){
+        return `You are ${wrapper}${quality}${wrapper}!`
+    }
+}
+
+
